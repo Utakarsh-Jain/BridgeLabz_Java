f1344d72153ef2aa8bc3502fb334df9082673b35
Utakarsh-Jain <utakarshjain2@gmail.com>
2026-09-22 02:38:18 +0530

    [Utakarsh Jain] : Added Solutions of Level 3 of Java Control Flow

---
+++ Bridgelabz_Java/src/main/java/JavaControlFlow/Level 3/CountDigits.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+/*
+Problem 2 - GCR Control Flow Level 3
+Create a program to count the number of digits in an integer.
+Hint => 
+Get an integer input for the number variable.
+Create an integer variable count with value 0.
+Use a loop to iterate until number is not equal to 0.
+Remove the last digit from number in each iteration
+Increase count by 1 in each iteration.
+Finally display the count to show the number of digits
+
+* Author : <Utakarsh Jain>
+* Date : 21-09-2026
+*/
+class CountDigits {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer:");
+        int number = sc.nextInt();
+        int count = 0;
+        if (number == 0) {
+            count = 1;
+        } else {
+            while (number != 0) {
+                number = number / 10;
+                count++;
+            }
+        }
+        System.out.println("Number of digits: " + count);
+        sc.close();
+    }
+}
